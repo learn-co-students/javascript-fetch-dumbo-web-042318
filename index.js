3fd05b69dca047048497e3e55ef763a1a050e0a3
jessebillard <jessebillard@mac.com>
2018-06-09 14:32:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+
+function fetchTest() {
+    fetch('https://api.github.com/users/octocat/repos')
+    .then(res => res.json())
+    .then(json => console.log(json))
+}
