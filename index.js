bf16b4554e5e7be45b74fd72ed50571f2ccf8e68
snkoski <shawnnkoski@gmail.com>
2018-06-12 13:21:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+// fetch('https://api.github.com/user/repos').
+//   then(res => res.json()).
+//   then(json => console.log(json))
+
+const token = 'e12c62dee8d6ce89f96d0703eb0bb2f473c75c95'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
